1ead8d25614ad51ce281372888487fbb8fb2bbca
m.yousefi <m.yousefi@titan-diesel.com>
2022-08-10 07:36:16 +0430

    decorator

--- src/Decorators/MethodDecorator.ts
+++ src/Decorators/MethodDecorator.ts
@@ -16,8 +16,29 @@ function second() {
     };
 }
 
+function catchError(target: any, propertyName: any, descriptor: any) {
+    const method = descriptor.value;
+
+    descriptor.value = async function (...args: any) {
+        try {
+            return await method.apply(target, args);
+        } catch (error) {
+            console.log(`${target.constructor.name}#${propertyName}`);
+            console.log(`catched error: ${error.message}`);
+        }
+    };
+}
+
 export class ExampleClass {
-    @first()
-    @second()
-    method() { }
+    //@first()
+    //@second()
+    @catchError
+    method() {
+        let rnd: Number;
+        rnd = (new Date()).getTime() % 10;
+        if (rnd < 5)
+            throw new Error("there is error here");
+        else
+            console.log("Ok!");
+    }
 }
